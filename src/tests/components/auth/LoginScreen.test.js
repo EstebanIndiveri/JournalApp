432eb7b8a7624f8f686a4811235dac238a582f40
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-09-05 18:53:50 -0300

    login google test

--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -6,8 +6,13 @@ import { mount } from 'enzyme';
 import LoginScreen from '../../../components/authentication/LoginScreen';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import { startGoogleLogin,startLoginEmailPassword } from '../../../actions/auth';
 
+jest.mock('../../../actions/auth',()=>({
+    startGoogleLogin:jest.fn(),
+    startLoginEmailPassword:jest.fn(),
 
+}))
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const initState={
@@ -19,6 +24,7 @@ const initState={
 };
 
 let store = mockStore(initState);
+store.dispatch=jest.fn();
 
 const wrapper=mount(
     <Provider store={store}>
@@ -30,15 +36,24 @@ const wrapper=mount(
 )
 
 describe('pruebas en LoginScreen', () => {
-    
-
     beforeEach(()=>{
         store = mockStore(initState);
+        jest.clearAllMocks();
     });
-    
     test('debe de mostrarse correctamente ', () => {
         expect(wrapper).toMatchSnapshot();
-        
-    })
+    });
+    test('debe disparar action de StartGoogleLogin', () => {
+        wrapper.find('.google-btn').prop('onClick')();
+        expect(startGoogleLogin).toHaveBeenCalled();
+    });
+    test('debe disparar startlogin', () => {
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        });
+    expect(startLoginEmailPassword).toHaveBeenCalledWith('','');
+    });
+    
+    
     
 })
